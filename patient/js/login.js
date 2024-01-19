d799f3e28bf8bcd091ae5b8e3a69d2cdedfa4b7e
saket-barevadia <saket.barevadia@etatvasoft.com>
2024-01-19 18:26:48 +0530

    adding files

--- patient/js/login.js
+++ patient/js/login.js
@@ -1,18 +1,60 @@
 // forgot password
 
-document.getElementById("click").addEventListener("click",()=>{
-    return [
-        fetch("forgotPass.html")
-        .then(response=>response.text())
-        .then(data=>document.getElementById("wrapper").innerHTML+=data),
-        document.getElementById("display").style.display="none"
+// document.getElementById("click").addEventListener("click",()=>{
+//     return [
+//         fetch("forgotPass.html")
+//         .then(response=>response.text())
+//         .then(data=>{ document.getElementById("wrapper").innerHTML+=data;
+//     addListners();}),
+//         document.getElementById("display").style.display="none",
+//     ]
+// })
+
+// function addListners(){
+//     console.log("load");
+//     console.log(document.getElementById("displayPass"));
+//     console.log(document);
+//     console.log(document.getElementById("display"));
+//     document.getElementById("back").addEventListener("click",()=>{
+//         return [
+//             document.getElementById("displayPass").style.display="none",
+//             document.getElementById("display").style.display="flex"
+//         ]
+//     })
+// }
+
+
+
+
+
+//change type 
+
+document.getElementById("password").addEventListener("click",()=>{
+    return[
+    document.querySelector("#changeType").type="text",
+    document.getElementById("password").style.display="none",
+    document.getElementById("password2").style.display="block"
+    ]
+})
+
+document.getElementById("password2").addEventListener("click",()=>{
+    return[
+    document.querySelector("#changeType").type="password",
+    document.getElementById("password2").style.display="none",
+    document.getElementById("password").style.display="block",
     ]
 })
 
 
-document.getElementById("back").addEventListener("click",()=>{
-    // return [
-    //     document.getElementById("displayPass").style.display="none",
-    //     document.getElementById("display").style.display="block"
-    // ]
+
+
+
+//dark mode
+document.getElementById("dark").addEventListener("click",()=>{
+    document.body.style.backgroundColor="rgb(27,26,26)"
 })
+
+
+
+
+// rgb(27, 26, 26)".color="white";
